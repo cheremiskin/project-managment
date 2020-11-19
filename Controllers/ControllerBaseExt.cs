558482c4d8fa5e15a300a0c823b3f4c3280cc36f
Kovalev Danil <opana.loog@gmail.com>
2020-11-19 12:15:27 +0300

    controller base ext added

---
+++ Controllers/ControllerBaseExt.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using pm.Models;
+using project_managment.Data.Repositories;
+
+namespace project_managment.Controllers
+{
+    public class ControllerBaseExt : ControllerBase
+    {
+        protected readonly IUserRepository _userRepository;
+
+        public ControllerBaseExt(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        protected User GetClientUser()
+        {
+            var email = User.Identity.Name;
+            return email == null ? null : _userRepository.FindUserByEmail(email).Result;
+        }
+
+        protected Claim GetClientRoleClaim()
+        {
+            return User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+        }
+        
+    }
+}
