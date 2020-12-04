3056c21a4528953be356793fc89eb25a99709ed2
Kovalev Danil <opana.loog@gmail.com>
2020-12-04 23:48:40 +0300

    log added

---
+++ Logging/RequestLogMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace project_managment.Logging
+{
+    public class RequestLogMiddleware 
+    {
+        private RequestDelegate _next;
+        private ILogger<RequestLogMiddleware> _log;
+
+        public RequestLogMiddleware(ILoggerFactory loggerFactory, RequestDelegate next)
+        {
+            _next = next; 
+            _log = loggerFactory.CreateLogger<RequestLogMiddleware>();
+        }
+        
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                _log.LogInformation("Request {method} {url} => {statusCode}",
+                    context.Request?.Method,
+                    context.Request?.Path.Value,
+                    context.Response?.StatusCode);
+                _log.LogDebug("debug test");
+                _log.LogTrace("trace test");
+            }
+        }
+    }
+}
