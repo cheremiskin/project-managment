ce67746a1982f75e0762dd98a17ef3c0ee7e952a
Kovalev Danil <opana.loog@gmail.com>
2020-12-23 19:30:52 +0300

    delete stock classes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
-import { FetchData } from './pages/FetchData';
-import { Counter } from './pages/Counter';
 import { ProjectDetail } from './pages/ProjectDetail';
 import { Projects } from './pages/Projects';
 import { User } from './pages/User';
@@ -16,8 +14,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/user/:id' component={User} />
         <Route path='/projects/' component={Projects} />
         <Route path='/project/:id' component={ProjectDetail} />
