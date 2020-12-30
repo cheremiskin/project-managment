af31378306beb0c9b041f94595985b31bf3767eb
Kovalev Danil <opana.loog@gmail.com>
2020-12-30 07:38:56 +0300

    add admin creation

---
+++ SQL/create_admin.sql
@@ -0,0 +1,2 @@
+﻿insert into users(email, full_name, password, role_id, info) values 
+('admin@admin.ru', 'Admin', '$2a$10$llw0G6IyibUob8h5XRt9xu8kZtTD7pCNgPW3J7/2rBJIVGJNoXzmy', 1, 'It is admin user');
